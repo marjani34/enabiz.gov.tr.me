54b598d92a103bd427624f336f89525c8dc6d48f
AMIR MARJANI <amir.marjani34@gmail.com>
2021-09-13 13:50:26 +0300

    LAST

--- src/App.js
+++ src/App.js
@@ -13,9 +13,9 @@ function App() {
         <p className="note note-success">
       <img src={checkmark} className="checkmark" alt="enabizLogo"/>
           
-          The result of this test belongs to  A*** MARJANIBEN*** with Passport Number *******7018.
+          The result of this test belongs to  M****** KEY*** with Passport Number *******4975.
           The test result is NEGATIVE and the sampling date is 13/9/2021 11:13:00 AM.<br/>
-          It was downloaded by  A*** MARJANIBEN*** on 13.09.2021.<br/>
+          It was downloaded by  M****** KEY*** on 13.09.2021.<br/>
           CAN BE USED AS A DOCUMENT FOR EXIT ABROAD.<br/>
         </p>
         <div className="clearfix"></div>
